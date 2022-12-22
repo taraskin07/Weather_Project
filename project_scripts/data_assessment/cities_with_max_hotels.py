62d691265f361f102800a743e843a929dc6bb708
taraskin07 <taraskin_07@bk.ru>
2022-12-22 06:14:44 +0300

    change_alg

--- project_scripts/data_assessment/cities_with_max_hotels.py
+++ project_scripts/data_assessment/cities_with_max_hotels.py
@@ -1,9 +1,137 @@
+def recursion_func(df, df_allocation, precision=2):
+
+
+    """
+    This function clean the initial dataset (param: df) from coordinates, related to different cities with the same name in the same country,
+    with the help of subset dataframe (param: df_allocation),
+    that contain information about each allocation (both country and city: ('Country', 'City') tuple, column "Allocation").
+
+    :param df: initial dataframe to be cleaned
+    :param df_allocation: dataframe - subset for specific allocation ('Country', 'City')
+    :param precision: float or integer - allowable error (integer or float degrees) in determining a coordinate as valid and related to the same city, default = 2 degrees
+    :return df: cleaned dataframe, without irrelevant rows for particular allocation
+    """
+    df_allocation = df_allocation.astype({"Latitude": float})
+    df_allocation = df_allocation.astype({"Longitude": float})
+
+    # Finding mean values to establish how differs each value of latitude and latitude from their average in group of coordinates for city with the same name/
+    latitude_mean = df_allocation["Latitude"].mean()
+
+    # Setting up precision. A range in which each value should be, in order to refer to the same city.
+    upper_lat = float(latitude_mean) + float(precision)
+    lower_lat = float(latitude_mean) - float(precision)
+
+    # Same for longitude.
+    longitude_mean = df_allocation["Longitude"].mean()
+
+    upper_lon = float(longitude_mean) + float(precision)
+    lower_lon = float(longitude_mean) - float(precision)
+
+    # For each coordinate in a subset for specific allocation it is necessary to establish whether it is inside the valid value range or not.
+    for index, row in df_allocation.iterrows():
+
+        latitude=row['Latitude']
+        longitude=row['Longitude']
+
+        # If some value is outside the valid range we need to understand what is the most common coordinate value.
+        if not lower_lat < float(latitude) < upper_lat:
+
+            # List for values in order to find the mosst common one.
+            max_difference_lat_list = []
+
+            # Difference for each value from the most common one.
+            max_difference_lat=0
+
+            # Finding the most common value of coordinate.
+            for index, row in df_allocation.iterrows():
+                lat = row['Latitude']
+                # Coordinates differs from each other, but if they will be rounded to the integer part, then for the most part they will be the same.
+                lat = round(lat, 0)
+                max_difference_lat_list.append(lat)
+            most_common_value_lat = max(set(max_difference_lat_list), key=max_difference_lat_list.count)
+
+            # Finding the value that differs the most from the most common coordinate
+            for index, row in df_allocation.iterrows():
+                value = abs(abs(row['Latitude'])-abs(most_common_value_lat))
+                if value > max_difference_lat:
+                    max_difference_lat = value
+                    # Both dataframes have column with IDs which identify the rows.
+                    # Eventually the ID for the most difference will be saved in 'id_m' variable.
+                    id_m = row['Id']
+
+            # This row (corresponding to specific ID) now deleted from both initial dataframe and its subset.
+            df_allocation = df_allocation[df_allocation.Id != id_m]
+            df = df[df.Id != id_m]
+
+            # Procedure need to be repeated to clear dataframe from all invalid coordinates step by step.
+            return recursion_func(df, df_allocation)
+
+        # If latitude coordinates now fine longitude values need to be checked.
+        elif not lower_lon < float(longitude) < upper_lon:
+
+            # List for values in order to find the mosst common one.
+            max_difference_lon_list = []
+
+            # Difference for each value from the most common one.
+            max_difference_lon = 0
+            for index, row in df_allocation.iterrows():
+                lon = row['Longitude']
+                lon = round(lon, 0)
+                max_difference_lon_list.append(lon)
+            most_common_value_lon = max(set(max_difference_lon_list), key=max_difference_lon_list.count)
+
+            # Finding the most common value of coordinate.
+            for index, row in df_allocation.iterrows():
+                value = abs(abs(row['Longitude'])-abs(most_common_value_lon))
+                if value > max_difference_lon:
+                    max_difference_lon = value
+                    id_m = row['Id']
+            # Again this row (corresponding to specific ID) now deleted from both initial dataframe and its subset.
+            df_allocation = df_allocation[df_allocation.Id != id_m]
+            df = df[df.Id != id_m]
+            return recursion_func(df, df_allocation)
+
+    # After all procedures the initial dataframe without irrelevant rows for particular allocation is returned.
+    return df
+
+def city_location_check(df, precision=2):
+
+
+    """
+    This function clean the initial dataset (param: df) from coordinates,
+    related to different cities with the same name
+    by constructing the subset dataframe for each unique allocation found,
+    (both country and city: ('Country', 'City') tuple, column "Allocation")
+    and start the process of cleaning with the help of 'recursion_func'.
+
+    :param df: initial dataframe to be cleaned
+    :param precision: integer - allowable error (integer degrees) in determining a coordinate as valid and related to the same city, default = 2 degrees
+    :return df: clean dataframe, without irrelevant rows
+    """
+
+
+    # Finding list of uniqie allocations.
+    df_allocation = df["Allocation"].unique()
+    df_allocation=list(map(tuple, df_allocation))
+
+    # Constructing a subset dataframe.
+    for allocation in df_allocation:
+        df_allocation = df.loc[df['Allocation'] == allocation]
+
+        # Launching the recursion_func for each allocation.
+        df = recursion_func(df, df_allocation)
+
+    return df
 def cities_with_max_amount_of_hotel(df):
+
+
     """
     This function finds cities with maximum amount of hotels in a particular country.
     :param df: dataframe
     :return df_sorted: sorted dataframe with cities that have maximum amount of hotels
     """
+
+
     # Getting object "Series", this object is sorted by country and city. Creating the dataframe from "Series".
     # Adding "Size" column with values that reflect amount od hotels in the city.
     df_new = df.groupby(["Country", "City"]).size().to_frame("Size").reset_index()
@@ -25,5 +153,10 @@ def cities_with_max_amount_of_hotel(df):
     df_sorted = df.loc[
         df["Allocation"].isin(top_cities_with_max_hotels_df["Allocation"].values)
     ]
-    df_sorted = df_sorted.sort_values(["Country"])
-    return df_sorted
+
+    # Launching the city_location_check function in order to clean dataframe from cities with the same name and country.
+    df_max_hotels = city_location_check(df_sorted)
+
+    return df_max_hotels
+
+
--- project_scripts/data_processing/addresses.py
+++ project_scripts/data_processing/addresses.py
@@ -9,26 +9,51 @@ from geopy.extra.rate_limiter import RateLimiter
 
 def geopy_address(coordinates_list, workers_amount, api_key):
     """
-    The function to get an address by coordinates. Allows you to set the number of threads.
+    The function to get an address by coordinates. Allows one to set the number of threads.
+
     :param workers_amount: number of threads for parallel data processing
     :param coordinates_list: list with coordinates
     :param api_key: API-key from https://developer.mapquest.com/
     :return df: dataframe with address and coordinates
     """
+
+    #Using MapQuest service.
     geolocator = MapQuest(api_key=api_key)
 
-    # Set a delay in seconds to avoid "Too Many Requests 429 error".
-    delay = 1 / 20
-    get_address = RateLimiter(geolocator.reverse, min_delay_seconds=delay)
+    # Counter for empty responses if any.
+    empty_address_counter = 0
+
+    def my_iterator(coord):
+        """
+        Additional function to get a proper response from API.
+
+        :param coord: latitude and longitude values from coordinates_list
+        :return str, tuple of str: "Address", ("Latitude", "Longitude")
+        """
+        # Set a delay in seconds to avoid "Too Many Requests 429 error".
+        delay = 1 / 20
+        get_address = RateLimiter(geolocator.reverse, min_delay_seconds=delay)
+        location = get_address(coord, exactly_one=True)
+        get_address = location.address
+        if get_address == '':
+            nonlocal empty_address_counter
+            empty_address_counter += 1
+            get_address = f'Empty response for address request #{empty_address_counter}'
+        latit, longit = coord.split(', ')
+        return get_address, (latit, longit)
 
     with ThreadPoolExecutor(max_workers=workers_amount) as th:
-        locations = dict(th.map(get_address, coordinates_list))
+        locations = dict(th.map(my_iterator, coordinates_list))
 
+    # Constructing dataframe from dictionary {"Address": ("Latitude", "Longitude")}
     df = pd.DataFrame(
         locations.values(), index=locations.keys(), columns=["Latitude", "Longitude"]
     )
     df["Address"] = df.index
+    df = df.astype({"Latitude": str})
+    df = df.astype({"Longitude": str})
     df = df.reset_index(drop=True)
+
     return df
 
 
@@ -70,6 +95,7 @@ def create_csv_file_with_addresses(df1, df2, path):
 def save_df_in_csv_less_than_100_notes(df, path):
     """
     The function for saving files to csv, no more than 100 entries in each.
+
     :param df: dataframe
     :param path: the path where the resulting csv file will be saved
     :return:
@@ -85,118 +111,3 @@ def save_df_in_csv_less_than_100_notes(df, path):
         )
 
 
-# if __name__ == '__main__':
-#
-#     api_key = 'W0oJzSg0aPkT3fAHlzAsSwKuIGeJvlOc'
-#     from getting_coordinates import get_coordinates_list
-#
-#     import pandas as pd
-#
-#     df = pd.read_csv('../../../test_files/DF_max_amount_of_hotels.csv', sep="\t", encoding="utf-8",)
-#
-#     df1 = df[223:240].reset_index(drop=True)
-#     print(df1)
-#
-#     coord = get_coordinates_list(df)
-#
-#     new_c1 = coord[223:240]
-#     # print(new_c1)
-#     # print(len(new_c1))
-#
-#     # # Combine two dataframes into one by adding columns to the final table.
-#     # # Some of the values are NaN.
-#     df2 = geopy_address(new_c1, 17, api_key)
-#     df = df1.combine_first(df2)
-#     print(df.columns)
-#
-#     # Remove rows with empty values.
-#     df = df.dropna(axis=0)
-#     unique_country_city = df["Allocation"].unique()
-#     print(df.columns)
-#
-#
-#
-#     # To make a folder "country/city",
-#     # an information is taken from "Allocation" column where the values are a tuple: (country, city).
-#     for names in unique_country_city:
-#
-#         data_proc = df.loc[df["Allocation"] == names]
-#         data_proc.reset_index(drop=True, inplace=True)
-#         del data_proc["Allocation"]
-#         del data_proc["City"]
-#         del data_proc["Country"]
-#         print(data_proc["Latitude"])
-#         # save_df_in_csv_less_than_100_notes(data_proc, output_folder_path)
-#
-#     # print(coord)
-#     # for i in range(len(coord)):
-#     #     if '48.8870573, 2.3143297' in coord[i]:
-#     #         ind = coord.index(coord[i])
-#     #         print(coord[ind])
-#     #         print(ind)
-#     # new_c2 = coord[341:347]
-#     # new_c = new_c1+new_c2
-#     # print(new_c)
-#     # print(len(new_c))
-#     # lat = []
-#     # lon = []
-#     # for rec in new_c:
-#     #     nrec = rec.split(", ")
-#     #     lat.append(nrec[0])
-#     #     lon.append(nrec[1])
-#     # print(lat)
-#     # print(lon)
-#     # # df2 = pd.DataFrame(columns=df.columns)
-#     # df2 = df[df['Latitude'].isin(lat) & df['Longitude'].isin(lon)]
-#     # print(df2.head())
-#     # print(len(df2))
-#     # for string in new_c:
-#     #     for i in range(len(df)):
-#     #         if string == f'{df.at[i, "Latitude"]}, {df.at[i, "Longitude"]}':
-#     #             concat_df = pd.concat([df2,df[i]])
-#     # print(concat_df)
-#
-#     # ndf = geopy_address(new_c, 23, api_key)
-#     # print(ndf.head(5))
-#     #
-#     # print(ndf[5:10])
-#     # print(ndf[:5])
-#     # print(ndf[10:15])
-#     # print(ndf[15:20])
-#     # print(ndf.tail(3))
-#
-#
-#
-#
-
-
-#     # coordinates_list = [[48.1955998, 16.3826989], [47.1955998, 15.3826989]]
-#     # geolocator = MapQuest(api_key='W0oJzSg0aPkT3fAHlzAsSwKuIGeJvlOc')
-#     #
-#     # # Set a delay in seconds to avoid "Too Many Requests 429 error".
-#     # delay = 1 / 20
-#     # get_address = RateLimiter(geolocator.reverse, min_delay_seconds=delay)
-#     #
-#     # with ThreadPoolExecutor(max_workers=2) as th:
-#     #     locations = dict(th.map(get_address, coordinates_list))
-#     locations = {'12 Rennweg, 3. Bezirk-Landstraße, Wien, Wien, 9, AT, 1030': (48.19596, 16.38288), 'Am Hiening, Semriach, Graz-Umgebung, 6, AT, 8102': (47.19543, 15.38107),'12  9, AT, 1030': (22.19896, 33.38288),}
-#
-#     # print (f'Locations keys are: \n{locations.keys()}')
-#     # print (f'Locations values are: \n{locations.values()}')
-#
-#     print(locations)
-#
-#     # Dictionary with coordinates and address is converted into a DataFrame.
-#     df = pd.DataFrame(
-#         locations.values(), index=locations.keys(), columns=["Latitude", "Longitude"]
-#     )
-#     df["Address"] = df.index
-#     df = df.reset_index(drop=True)
-#     print (f'Dataframe: \n\n{df.head()}')
-#
-#     df = pd.DataFrame.from_dict(locations, orient='index').reset_index()
-#     df = df.rename(columns = {"index":"Address", 0:"Latitude", 1:"Longitude"})
-#     print(f'Dataframe Address: \n\n{df.head()}')
-#     # # df["Address"] = df.index
-#     # #
-#     # print (f'Dataframe reset: \n\n{df.head()}')
